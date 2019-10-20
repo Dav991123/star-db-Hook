6786ac25c9a00f78dd10565b2a2e0a0e63bc2cad
Dav991123 <sargsyand89@gmail.com>
2019-10-20 18:27:25 +0400

    add spinner component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spiner';
 
 import './random-planet.css';
 const swapi = new SwapiService();
 
 const RandomPlanet = ()  =>  {
+    const [loading, setLoading] = useState(false)
     const [planet, setPlanet] = useState({});
     const uptadePlanet = () => {
+        setLoading(true);
         const id = Math.floor(Math.random() * 25)  + 2 ;
         swapi.getPlanet(id)
             .then(planet => {
+                setLoading(false)
                 setPlanet({...planet})
             })
     };
@@ -21,10 +25,12 @@ const RandomPlanet = ()  =>  {
     const { id, name, population, rotationPeriod, diameter } = planet;
     return (
         <div className="random-planet jumbotron rounded">
+            { loading && <Spinner />}
+
             <img className="planet-image"
                  src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
             <div>
-                <h4>Planet Name {name}</h4>
+                <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item">
                         <span className="term">Population</span>
